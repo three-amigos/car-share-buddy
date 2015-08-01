483da1ac432b0c0a77d9cc8299353d86402aec0f
Adnan Gazi <adnan.gazi01@gmail.com>
2015-08-01 16:36:36 +0100

    add in-memory user repository, move source into folders

--- car-share-buddy/UserRepository.swift
+++ car-share-buddy/UserRepository.swift
@@ -30,7 +30,62 @@ protocol UserRepository {
 class UserRepositoryFactory {
     
     class func getRepository() -> UserRepository {
-        return LocalUserRepository()
+        return ArrayUserRepository()
+    }
+    
+}
+
+class ArrayUserRepository: UserRepository {
+
+    private var userArray:[User] = []
+    
+    func getAll() -> [User] {
+        return userArray
+    }
+    
+    func getByName(name: String) -> User? {
+        
+        for user in userArray {
+            if user.name == name {
+                return user
+            }
+        }
+        
+        return nil
+    }
+    
+    func add(user: User) -> User? {
+        let prev = getByName(user.name)
+        
+        if prev != nil {
+            delete(prev!)
+        }
+        
+        userArray.append(user)
+        
+        return prev
+    }
+    
+    func delete(user: User) -> User? {
+        
+        for (index, u) in userArray.enumerate() {
+            if (u.name == user.name) {
+                userArray.removeAtIndex(index)
+                return user
+            }
+        }
+        
+        return nil
+    }
+    
+    func updateByName(user: User) -> User? {
+        if let prev = getByName(user.name) {
+            delete(prev)
+            userArray.append(user)
+            return prev
+        }
+        
+        return nil
     }
     
 }
